56d16f21c5fc6bfa2de2a66677b59538f7daf390
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-22 20:07:57 +0200

    app files task1

--- src/application/inputModels/blogInputModel.ts
+++ src/application/inputModels/blogInputModel.ts
@@ -1,4 +1,4 @@
-export class BlogInputModelDto {
+export class BlogInputModel {
   name: string; //*, maxLength: 15
   description: string; //*, maxLength: 500
   websiteUrl: string; //  *,  maxLength: 100, pattern: ^https://([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$
