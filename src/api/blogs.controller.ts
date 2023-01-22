c378b5b14f14d13e50a3786714f62ee226b44283
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-22 20:04:58 +0200

    app files task1

---
+++ src/api/blogs.controller.ts
@@ -0,0 +1,110 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  InternalServerErrorException,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { BlogsService } from '../application/blogs.service';
+import { BlogInputModelDto } from '../application/inputModels/blogInputModel.dto';
+import { castQueryParams } from '../infrastructure/helpers/helpers';
+import { QueryRepository } from '../infrastructure/repositories/query.repository';
+import { PaginatorInputType } from '../application/inputModels/paginatorInputType';
+import { PostViewModel } from '../infrastructure/viewModels/postViewModel';
+import { PaginatorView } from '../infrastructure/viewModels/paginatorView';
+import { BlogPostInputModelDto } from '../application/inputModels/blogPostInputModel.dto';
+import { PostsService } from '../application/posts.service';
+import { PostInputModelDto } from '../application/inputModels/postInputModel.dto';
+
+@Controller('blogs')
+export class BlogsController {
+  constructor(
+    private blogsService: BlogsService,
+    private postService: PostsService,
+    private queryRepository: QueryRepository,
+  ) {}
+  @Get()
+  async getBlogs(
+    @Query('searchNameTerm') searchNameTerm: string | null = null,
+    @Query() query,
+  ) {
+    const paginatorParams = castQueryParams(query);
+    return await this.queryRepository.findBlogs(
+      paginatorParams,
+      searchNameTerm,
+    );
+  }
+
+  @Post()
+  async createBlog(@Body() blog: BlogInputModelDto) {
+    return await this.blogsService.createNewBlog(blog);
+  }
+
+  @Get(':blogId/posts')
+  async getPostsForBlog(
+    @Param('blogId') blogId: string,
+    @Query() query: PaginatorInputType,
+  ): Promise<PaginatorView<PostViewModel>> {
+    if (!(await this.queryRepository.checkBlogId(blogId))) {
+      throw new NotFoundException('Invalid blogId');
+    }
+    return await this.queryRepository.findPosts(query, blogId);
+  }
+
+  @Post(':blogId/posts')
+  async createPostForBlog(
+    @Param('blogId') blogId: string,
+    @Body() blogPostDto: BlogPostInputModelDto,
+  ): Promise<PostViewModel> {
+    if (!(await this.queryRepository.checkBlogId(blogId))) {
+      throw new NotFoundException('Invalid blogId');
+    }
+    const createdPost: PostInputModelDto = {
+      title: blogPostDto.title,
+      shortDescription: blogPostDto.shortDescription,
+      content: blogPostDto.content,
+      blogId,
+    };
+    return this.postService.createNewPost(createdPost);
+  }
+
+  @Get(':blogId')
+  async getBlog(@Param('blogId') blogId: string) {
+    const blog = await this.queryRepository.getBlogById(blogId);
+    if (!blog) {
+      throw new NotFoundException('Invalid blogId');
+    }
+    return blog;
+  }
+
+  @Put(':blogId')
+  @HttpCode(204)
+  async editBlog(
+    @Param('blogId') blogId: string,
+    @Body() changes: BlogInputModelDto,
+  ) {
+    if (!(await this.queryRepository.checkBlogId(blogId))) {
+      throw new NotFoundException('Invalid blogId');
+    }
+    return await this.blogsService.editBlog(blogId, changes);
+  }
+
+  @Delete(':blogId')
+  @HttpCode(204)
+  async deleteBlog(@Param('blogId') blogId: string) {
+    if (!(await this.queryRepository.checkBlogId(blogId))) {
+      throw new NotFoundException('Invalid blogId');
+    }
+    const result = await this.blogsService.deleteBlog(blogId);
+    if (!result) {
+      throw new InternalServerErrorException('Blog not deleted');
+    }
+    return;
+  }
+}
