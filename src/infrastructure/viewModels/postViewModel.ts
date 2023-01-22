78e43917b87a745aa110c559d87e58364ffd2ccc
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-22 20:11:34 +0200

    app files task1

---
+++ src/infrastructure/viewModels/postViewModel.ts
@@ -0,0 +1,12 @@
+import { ExtendedLikesInfoViewModel } from './extendedLikesInfoViewModel';
+
+export interface PostViewModel {
+  id: string;
+  title: string;
+  shortDescription: string;
+  content: string;
+  blogId: string;
+  blogName: string;
+  createdAt: string;
+  extendedLikesInfo: ExtendedLikesInfoViewModel;
+}
