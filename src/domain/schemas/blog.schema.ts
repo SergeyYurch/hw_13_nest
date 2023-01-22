114661c520a7152c583cd80d5b94c95241dce878
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-22 20:04:47 +0200

    app files task1

---
+++ src/domain/schemas/blog.schema.ts
@@ -0,0 +1,44 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, Types } from 'mongoose';
+import { BlogViewModel } from '../../infrastructure/viewModels/blogViewModel';
+import { BlogInputModelDto } from '../../application/inputModels/blogInputModel.dto';
+
+@Schema()
+export class Blog {
+  _id: Types.ObjectId;
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ required: true })
+  websiteUrl: string;
+
+  @Prop({ default: new Date() })
+  createdAt: Date;
+
+  getViewModel(): BlogViewModel {
+    return {
+      id: this._id.toString(),
+      name: this.name,
+      description: this.description,
+      websiteUrl: this.websiteUrl,
+      createdAt: this.createdAt.toISOString(),
+    };
+  }
+
+  changesApply(changes: BlogInputModelDto) {
+    for (const key in changes) {
+      this[key] = changes[key];
+    }
+  }
+}
+export const BlogSchema = SchemaFactory.createForClass(Blog);
+BlogSchema.methods = {
+  getViewModel: Blog.prototype.getViewModel,
+  changesApply: Blog.prototype.changesApply,
+};
+
+export type BlogDocument = HydratedDocument<Blog>;
