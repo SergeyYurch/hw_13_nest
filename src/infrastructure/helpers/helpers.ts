d627a177383d039433bf522ef5ebd7b714d484e5
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-29 22:43:18 +0200

    add new functions

--- src/infrastructure/helpers/helpers.ts
+++ src/infrastructure/helpers/helpers.ts
@@ -1,13 +1,6 @@
-import { PaginatorInputType } from '../../application/inputModels/paginatorInputType';
-import * as bcrypt from 'bcrypt';
-import { hash } from 'bcrypt';
+import { PaginatorInputType } from '../../api/inputModels/paginatorInputType';
+
 import { v4 as uuidv4 } from 'uuid';
-import {
-  CONFIRM_EMAIL_LIFE_PERIOD,
-  CONFIRM_EMAIL_LIFE_PERIOD_Num,
-  RECOVERY_PASSWORD_CODE_LIFE_PERIOD,
-} from '../../settings-const';
-import add from 'date-fns/add';
 
 export const castQueryParams = (query): PaginatorInputType => {
   const queryParams = new PaginatorInputType();
@@ -20,29 +13,25 @@ export const castQueryParams = (query): PaginatorInputType => {
 
 export const pagesCount = (totalCount: number, pageSize: number) =>
   Math.ceil(totalCount / pageSize);
-
-export const generatePassHash = async (
-  password: string,
-  salt: string,
-): Promise<string> => {
-  return await hash(password, salt);
-};
-
-export const generateHashSalt = async (): Promise<string> => {
-  const salt_base = Number(process.env.HASH_SALT_BASE) || 10;
-  return await bcrypt.genSalt(salt_base);
-};
+//
+// export const generatePassHash = async (
+//   password: string,
+//   salt: string,
+// ): Promise<string> => {
+//   return await hash(password, salt);
+// };
+//
+// export const generateHashSalt = async (): Promise<string> => {
+//   const salt_base = Number(process.env.HASH_SALT_BASE) || 10;
+//   return await bcrypt.genSalt(salt_base);
+// };
 
 export const getConfirmationCode = (): string => uuidv4();
 
 export const getConfirmationEmailExpirationDate = () =>
-  new Date(CONFIRM_EMAIL_LIFE_PERIOD_Num + Date.now());
-// add(new Date(), {
-//   [CONFIRM_EMAIL_LIFE_PERIOD.units]: CONFIRM_EMAIL_LIFE_PERIOD.amount,
-// });
+  new Date(+process.env.CONFIRM_EMAIL_LIFE_PERIOD_SEC * 1000 + Date.now());
 
-export const getRecoveryPasswordCodeExpirationDate = () =>
-  add(new Date(), {
-    [RECOVERY_PASSWORD_CODE_LIFE_PERIOD.units]:
-      RECOVERY_PASSWORD_CODE_LIFE_PERIOD.amount,
-  });
+export const getPasswordRecoveryCodeExpirationDate = () =>
+  new Date(
+    +process.env.PASSWORD_RECOVERY_CODE_LIFE_PERIOD_SEC * 1000 + Date.now(),
+  );
