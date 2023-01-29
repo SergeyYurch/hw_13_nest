f428f0dfce62e5feac774c24fa94d95dbf2a43e4
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-29 22:42:40 +0200

    add new functions

--- src/query/viewModels/commentViewModel.ts
+++ src/query/viewModels/commentViewModel.ts
@@ -3,8 +3,10 @@ import { LikesInfoViewModel } from './likesInfoViewModel';
 export interface CommentViewModel {
   id: string;
   content: string;
-  userId: string;
-  userLogin: string;
+  commentatorInfo: {
+    userId: string;
+    userLogin: string;
+  };
   createdAt: string;
   likesInfo: LikesInfoViewModel;
 }
