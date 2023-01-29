c1e51324304d424b585395c36d627ecef7c5b615
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-29 22:44:25 +0200

    add new functions

---
+++ src/posts/domain/like-for-post.schema.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { LikeStatusType } from '../../api/inputModels/likeInputModel';
+
+@Schema()
+export class LikeForPost {
+  @Prop({ required: true })
+  userId: string;
+
+  @Prop()
+  login: string;
+
+  @Prop({ required: false, default: false })
+  userBan: boolean;
+
+  @Prop({ required: true })
+  likeStatus: LikeStatusType;
+
+  @Prop({ required: true })
+  addedAt: Date;
+
+  @Prop({ required: true })
+  updatedAt: Date;
+}
+export const LikeForPostSchema = SchemaFactory.createForClass(LikeForPost);
