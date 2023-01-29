1517cfd4622d772abc20fe473993fc3cd7415f20
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-29 22:44:58 +0200

    add new functions

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './api/exceptionFilters/http-exception.filter';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(cookieParser());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      stopAtFirstError: true,
+      exceptionFactory: (errors) => {
+        const errorsForResponse = [];
+        for (const e of errors) {
+          const key = Object.keys(e.constraints)[0];
+          errorsForResponse.push({
+            message: e.constraints[key],
+            field: e.property,
+          });
+        }
+        throw new BadRequestException(errorsForResponse);
+      },
+    }),
+  );
+  app.useGlobalFilters(new HttpExceptionFilter());
   const port = process.env.PORT || 5000;
   await app.listen(port);
 }
