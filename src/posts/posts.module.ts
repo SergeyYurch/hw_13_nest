c891584c4d39064211e33c73ee7fa687fd7a0d57
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-31 23:23:02 +0200

    add security module

---
+++ src/posts/posts.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Blog, BlogSchema } from '../blogs/domain/blog.schema';
+import { Post, PostSchema } from './domain/post.schema';
+import { PostsController } from './posts.controller';
+import { PostsService } from './posts.service';
+import { PostsRepository } from './posts.repository';
+import { QueryModule } from '../query/query.module';
+import { CommentsModule } from '../comments/comments.module';
+import { CommentsService } from '../comments/comments.service';
+import { Comment, CommentSchema } from '../comments/domain/comment.schema';
+import { CommentsRepository } from '../comments/comments.repository';
+
+@Module({
+  imports: [
+    QueryModule,
+    CommentsModule,
+    MongooseModule.forFeature([
+      { name: Post.name, schema: PostSchema },
+      { name: Blog.name, schema: BlogSchema },
+      { name: Comment.name, schema: CommentSchema },
+    ]),
+  ],
+  controllers: [PostsController],
+  providers: [
+    PostsService,
+    PostsRepository,
+    CommentsService,
+    CommentsRepository,
+  ],
+  exports: [PostsService],
+})
+export class PostsModule {}
