e61aae340900652ff7bb00121e01af90600413e1
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-31 23:23:47 +0200

    add security module

---
+++ src/security/security.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Delete,
+  UseGuards,
+  Req,
+  HttpCode,
+} from '@nestjs/common';
+import { SecurityService } from './security.service';
+import { RefreshTokenGuard } from '../api/guards/refresh-token.guard';
+import { Request } from 'express';
+
+@UseGuards(RefreshTokenGuard)
+@Controller('security')
+export class SecurityController {
+  constructor(private readonly securityService: SecurityService) {}
+
+  @Get('devices')
+  getDeviceSessions(@Req() req: Request) {
+    const userId = req.user.userId;
+    return this.securityService.getAllSessionByUserId(userId);
+  }
+
+  @HttpCode(204)
+  @Delete('devices')
+  async deleteOtherDeviceSessions(@Req() req: Request) {
+    const { deviceId, userId } = req.user;
+    return await this.securityService.deleteAllSessionExcludeCurrent(
+      deviceId,
+      userId,
+    );
+  }
+
+  @HttpCode(204)
+  @Delete('devices/:deviceId')
+  async deleteDeviceSession(
+    @Param('deviceId') deviceId: string,
+    @Req() req: Request,
+  ) {
+    const { userId } = req.user;
+    await this.securityService.validateOwner(userId, deviceId);
+    await this.securityService.deleteSessionById(deviceId, userId);
+  }
+}
