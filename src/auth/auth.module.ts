e7614a51b9b32e6fd469166224dfa1fe134e4661
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-31 23:22:33 +0200

    add security module

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,41 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { QueryModule } from '../query/query.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AccessTokenStrategy } from './strategies/access-token.strategy';
+import { RefreshTokenStrategy } from './strategies/refresh-token.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from '../users/users.module';
+import { UsersRepository } from '../users/users.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from '../users/domain/user.schema';
+import { LocalStrategy } from './strategies/local.strategy';
+import { UsersService } from '../users/users.service';
+import { MailService } from '../infrastructure/mail.service/mail.service';
+import { BasicStrategy } from './strategies/auth-basic.strategy';
+
+@Module({
+  imports: [
+    QueryModule,
+    UsersModule,
+    ConfigModule,
+    PassportModule,
+    JwtModule.register({}),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  controllers: [AuthController],
+  providers: [
+    AuthService,
+    UsersService,
+    MailService,
+    ConfigService,
+    AccessTokenStrategy,
+    RefreshTokenStrategy,
+    LocalStrategy,
+    BasicStrategy,
+    UsersRepository,
+  ],
+})
+export class AuthModule {}
