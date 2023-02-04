5274216914cbd48ca1f99cf93a986109cb7efc90
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-04 15:37:10 +0200

    fix errors with validations

--- src/posts/dto/postInputModel.ts
+++ src/posts/dto/postInputModel.ts
@@ -1,18 +1,26 @@
-import { IsString, Length } from 'class-validator';
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+import { IsBlogExist } from '../common/blog-validate';
+import { Transform } from 'class-transformer';
 
 export class PostInputModel {
   @IsString()
+  @Transform(({ value }) => value.trim())
   @Length(1, 30)
+  @IsNotEmpty()
   title: string; // *, maxLength: 30
 
   @IsString()
+  @Transform(({ value }) => value.trim())
   @Length(1, 100)
   shortDescription: string; // * , maxLength: 100
 
   @IsString()
+  @Transform(({ value }) => value.trim())
   @Length(1, 1000)
   content: string; // *,  maxLength: 1000
 
   @IsString()
+  @Transform(({ value }) => value.trim())
+  @IsBlogExist({ message: 'blog does not exist' })
   blogId: string; // *
 }
