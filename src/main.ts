314c2e0fca9b4d2f40d481124e37da5ef528c1b3
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-04 15:38:47 +0200

    fix errors with validations

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,18 @@ import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './api/exceptionFilters/http-exception.filter';
 import cookieParser from 'cookie-parser';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
   app.set('trust proxy', 1);
   app.use(cookieParser());
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
   app.useGlobalPipes(
     new ValidationPipe({
       stopAtFirstError: true,
+      transform: true,
       exceptionFactory: (errors) => {
         const errorsForResponse = [];
         for (const e of errors) {
