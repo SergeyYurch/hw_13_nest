97d4c101507820d6eefdc3f068d190dd1ad121ff
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-04 15:37:54 +0200

    fix errors with validations

--- src/users/dto/userInputModel.ts
+++ src/users/dto/userInputModel.ts
@@ -1,9 +1,13 @@
 import { IsEmail, IsString, Length, Matches } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsUniqLoginOrEmail } from '../common/login-or-emai-uniq-validate';
 
 export class UserInputModel {
   @IsString()
+  @Transform(({ value }) => value.trim())
   @Length(3, 10)
   @Matches(/^[a-zA-Z0-9_-]*$/)
+  @IsUniqLoginOrEmail({ message: 'Login is already taken' })
   login: string;
 
   @IsString()
@@ -11,5 +15,7 @@ export class UserInputModel {
   password: string;
 
   @IsEmail()
+  @Transform(({ value }) => value.trim())
+  @IsUniqLoginOrEmail({ message: 'Email is already taken' })
   email: string;
 }
