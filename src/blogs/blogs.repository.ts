fab85ec1a1dccbaaabcfb5e8799d1a3a78043166
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-06 15:37:49 +0200

    refactoring to useCases & new endpoints

--- src/blogs/blogs.repository.ts
+++ src/blogs/blogs.repository.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@nestjs/common';
-import { Blog } from './domain/blog.schema';
+import { Blog, BlogDocument } from './domain/blog.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose';
 
 @Injectable()
 export class BlogsRepository {
+  constructor(@InjectModel(Blog.name) private BlogModel: Model<BlogDocument>) {}
+
+  async getBlogModel(id: string) {
+    return this.BlogModel.findById(id);
+  }
+
+  async createBlogModel() {
+    return new this.BlogModel();
+  }
+
+  async deleteBlog(blogId) {
+    const result = await this.BlogModel.deleteOne({
+      _id: new Types.ObjectId(blogId),
+    });
+    return result.deletedCount === 1;
+  }
+
   async save(blog): Promise<Blog> {
     return blog.save();
   }
