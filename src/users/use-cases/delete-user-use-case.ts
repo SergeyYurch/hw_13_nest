ff7cd54a18df7cf3e99d8444de50a7fb1e57cf57
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-06 15:39:02 +0200

    refactoring to useCases & new endpoints

---
+++ src/users/use-cases/delete-user-use-case.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from '../domain/user.schema';
+import { Model, Types } from 'mongoose';
+import { CommandHandler } from '@nestjs/cqrs';
+
+export class DeleteUserCommand {
+  constructor(public userId: string) {}
+}
+
+@CommandHandler(DeleteUserCommand)
+export class DeleteUserUseCase {
+  constructor(@InjectModel(User.name) private UserModel: Model<UserDocument>) {}
+
+  async execute(command: DeleteUserCommand): Promise<boolean> {
+    const result = await this.UserModel.deleteOne({
+      _id: new Types.ObjectId(command.userId),
+    });
+    return result.deletedCount === 1;
+  }
+}
