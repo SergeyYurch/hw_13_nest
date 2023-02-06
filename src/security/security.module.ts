3e7a3f8d9acd1c0668f026a24041b803a8921a0c
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-06 15:42:16 +0200

    refactoring to useCases & new endpoints

--- src/security/security.module.ts
+++ src/security/security.module.ts
@@ -4,33 +4,14 @@ import { SecurityController } from './security.controller';
 import { UsersModule } from '../users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from '../users/domain/user.schema';
-import { UsersService } from '../users/users.service';
-import { UsersRepository } from '../users/users.repository';
-import { QueryModule } from '../query/query.module';
-import { QueryRepository } from '../query/query.repository';
-import { MailService } from '../infrastructure/mail.service/mail.service';
-import { Post, PostSchema } from '../posts/domain/post.schema';
-import { Comment, CommentSchema } from '../comments/domain/comment.schema';
-import { Blog, BlogSchema } from '../blogs/domain/blog.schema';
+import { MailService } from '../common/mail.service/mail.service';
 
 @Module({
   imports: [
-    QueryModule,
     UsersModule,
-    MongooseModule.forFeature([
-      { name: User.name, schema: UserSchema },
-      { name: Post.name, schema: PostSchema },
-      { name: Comment.name, schema: CommentSchema },
-      { name: Blog.name, schema: BlogSchema },
-    ]),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [SecurityController],
-  providers: [
-    SecurityService,
-    UsersService,
-    UsersRepository,
-    QueryRepository,
-    MailService,
-  ],
+  providers: [SecurityService, MailService],
 })
 export class SecurityModule {}
