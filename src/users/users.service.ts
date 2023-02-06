35edc700eca2093d7472771b232ce0c3df0ef591
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-06 15:42:37 +0200

    refactoring to useCases & new endpoints

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,37 +3,35 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
 import { UsersRepository } from './users.repository';
 import { User, UserDocument } from './domain/user.schema';
-import { UserInputModel } from './dto/userInputModel';
-import { QueryRepository } from '../query/query.repository';
-import { UserViewModel } from '../query/viewModels/userViewModel';
-import { MailService } from '../infrastructure/mail.service/mail.service';
+import { MailService } from '../common/mail.service/mail.service';
+import { UsersQueryRepository } from './users.query.repository';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectModel(User.name) private UserModel: Model<UserDocument>,
     private readonly userRepository: UsersRepository,
-    private readonly queryRepository: QueryRepository,
+    private readonly usersQueryRepository: UsersQueryRepository,
     private readonly mailService: MailService,
   ) {}
 
-  async createNewUser(
-    userDto: UserInputModel,
-    isConfirmed?: boolean,
-  ): Promise<UserViewModel | null> {
-    const createdUser = new this.UserModel();
-    await createdUser.initialize(userDto, isConfirmed);
-    const user = await this.userRepository.save(createdUser);
-    if (!user) return null;
-    if (!isConfirmed) {
-      await this.mailService.sendConfirmationEmail(
-        user.accountData.email,
-        user.emailConfirmation.confirmationCode,
-      );
-    }
-    console.log(`${new Date()}user ${userDto.login} is register`);
-    return this.queryRepository.getUserViewModel(user);
-  }
+  // async createNewUser(
+  //   userDto: UserInputModel,
+  //   isConfirmed?: boolean,
+  // ): Promise<UserViewModel | null> {
+  //   const createdUser = new this.UserModel();
+  //   await createdUser.initialize(userDto, isConfirmed);
+  //   const user = await this.userRepository.save(createdUser);
+  //   if (!user) return null;
+  //   if (!isConfirmed) {
+  //     await this.mailService.sendConfirmationEmail(
+  //       user.accountData.email,
+  //       user.emailConfirmation.confirmationCode,
+  //     );
+  //   }
+  //   console.log(`${new Date()}user ${userDto.login} is register`);
+  //   return this.usersQueryRepository.getUserViewModel(user);
+  // }
 
   async deleteUser(userId: string): Promise<boolean> {
     const result = await this.UserModel.deleteOne({
