93ea710461ebd68f784b86eb692e86c058e8e056
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-06 15:38:22 +0200

    refactoring to useCases & new endpoints

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -1,17 +1,27 @@
 import { Module } from '@nestjs/common';
 import { CommentsController } from './comments.controller';
-import { QueryModule } from '../query/query.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Comment, CommentSchema } from './domain/comment.schema';
 import { CommentsService } from './comments.service';
 import { CommentsRepository } from './comments.repository';
+import { CommentsQueryRepository } from './comments.query.repository';
+import { UsersQueryRepository } from '../users/users.query.repository';
+import { User, UserSchema } from '../users/domain/user.schema';
 
 @Module({
   imports: [
-    QueryModule,
-    MongooseModule.forFeature([{ name: Comment.name, schema: CommentSchema }]),
+    MongooseModule.forFeature([
+      { name: Comment.name, schema: CommentSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
   ],
   controllers: [CommentsController],
-  providers: [CommentsService, CommentsRepository],
+  providers: [
+    CommentsService,
+    CommentsRepository,
+    CommentsQueryRepository,
+    UsersQueryRepository,
+  ],
+  exports: [CommentsService, CommentsRepository, CommentsQueryRepository],
 })
 export class CommentsModule {}
