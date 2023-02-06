284077e8d1c28e3a891e9265b6db6e1baa1e7172
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-06 15:37:39 +0200

    refactoring to useCases & new endpoints

--- src/blogs/blogs.module.ts
+++ src/blogs/blogs.module.ts
@@ -5,15 +5,36 @@ import { BlogsRepository } from './blogs.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Blog, BlogSchema } from './domain/blog.schema';
 import { PostsModule } from '../posts/posts.module';
-import { QueryModule } from '../query/query.module';
+import { BlogsQueryRepository } from './blogs.query.repository';
+import { Post, PostSchema } from '../posts/domain/post.schema';
+import { SaBlogsController } from './sa-blogs.controller';
+import { UsersModule } from '../users/users.module';
+import { CqrsModule } from '@nestjs/cqrs';
+import { BindBlogWithUserUseCase } from './use-cases/bind-blog-with-user-use-case';
+import { BloggerBlogsController } from './blogger-blogs.controller';
+import { CreateNewBlogUseCase } from './use-cases/create-new-blog-use-case';
+import { EditBlogUseCase } from './use-cases/edit-blog-use-case';
+import { DeleteBlogUseCase } from './use-cases/delete-blog-use-case';
+
+const useCases = [
+  BindBlogWithUserUseCase,
+  CreateNewBlogUseCase,
+  EditBlogUseCase,
+  DeleteBlogUseCase,
+];
 
 @Module({
   imports: [
-    QueryModule,
+    CqrsModule,
     PostsModule,
-    MongooseModule.forFeature([{ name: Blog.name, schema: BlogSchema }]),
+    UsersModule,
+    MongooseModule.forFeature([
+      { name: Blog.name, schema: BlogSchema },
+      { name: Post.name, schema: PostSchema },
+    ]),
   ],
-  controllers: [BlogsController],
-  providers: [BlogsService, BlogsRepository],
+  controllers: [BlogsController, SaBlogsController, BloggerBlogsController],
+  providers: [...useCases, BlogsService, BlogsRepository, BlogsQueryRepository],
+  exports: [BlogsQueryRepository],
 })
 export class BlogsModule {}
