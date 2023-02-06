211f710c46077600d80e8de010202d8bbcde99a4
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-06 15:36:50 +0200

    refactoring to useCases & new endpoints

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,29 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
-import { QueryModule } from '../query/query.module';
+import { AuthService } from './application/auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AccessTokenStrategy } from './strategies/access-token.strategy';
 import { RefreshTokenStrategy } from './strategies/refresh-token.strategy';
 import { PassportModule } from '@nestjs/passport';
-import { UsersModule } from '../users/users.module';
-import { UsersRepository } from '../users/users.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from '../users/domain/user.schema';
 import { LocalStrategy } from './strategies/local.strategy';
-import { UsersService } from '../users/users.service';
-import { MailService } from '../infrastructure/mail.service/mail.service';
+import { MailService } from '../common/mail.service/mail.service';
 import { BasicStrategy } from './strategies/auth-basic.strategy';
 import { ThrottlerModule } from '@nestjs/throttler';
+import { CommandBus, CqrsModule } from '@nestjs/cqrs';
+import { UsersModule } from '../users/users.module';
+import { CreateNewUserUseCase } from '../users/use-cases/create-new-user-use-case';
 
 @Module({
   imports: [
+    CqrsModule,
     ThrottlerModule.forRoot({
       ttl: 10,
       limit: 5,
     }),
-    QueryModule,
     UsersModule,
     ConfigModule,
     PassportModule,
@@ -33,14 +32,13 @@ import { ThrottlerModule } from '@nestjs/throttler';
   controllers: [AuthController],
   providers: [
     AuthService,
-    UsersService,
     MailService,
     ConfigService,
     AccessTokenStrategy,
     RefreshTokenStrategy,
     LocalStrategy,
     BasicStrategy,
-    UsersRepository,
+    CommandBus,
   ],
 })
 export class AuthModule {}
