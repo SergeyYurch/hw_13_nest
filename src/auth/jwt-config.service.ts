6e928aeda053087644dbaaeb5f1aa099e5f71935
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-10 15:47:49 +0200

    refactoring to useCases & new endpoints

--- src/auth/jwt-config.service.ts
+++
@@ -1,9 +0,0 @@
-import { JwtModuleOptions, JwtOptionsFactory } from '@nestjs/jwt';
-
-class JwtConfigService implements JwtOptionsFactory {
-  createJwtOptions(): JwtModuleOptions {
-    return {
-      secret: 'hard!to-guess_secret',
-    };
-  }
-}
