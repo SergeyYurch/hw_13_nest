acc0fcc5ef21b1f1fdacf46ad19005948be7e165
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-10 15:46:09 +0200

    refactoring to useCases & new endpoints

---
+++ src/comments/use-cases/create-comment-use-case.ts
@@ -0,0 +1,29 @@
+import { UsersQueryRepository } from '../../users/users.query.repository';
+import { CommentsRepository } from '../comments.repository';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+
+export class CreateCommentCommand {
+  constructor(
+    public content: string,
+    public userId: string,
+    public postId: string,
+  ) {}
+}
+@CommandHandler(CreateCommentCommand)
+export class CreateCommentUseCase
+  implements ICommandHandler<CreateCommentCommand>
+{
+  constructor(
+    protected usersQueryRepository: UsersQueryRepository,
+    protected commentsRepository: CommentsRepository,
+  ) {}
+
+  async execute(command: CreateCommentCommand) {
+    const { userId, postId, content } = command;
+    const { login } = await this.usersQueryRepository.getUserById(userId);
+    const commentModel = await this.commentsRepository.createCommentModel();
+    commentModel.initial(content, userId, login, postId);
+    await this.commentsRepository.save(commentModel);
+    return await this.commentsRepository.save(commentModel);
+  }
+}
