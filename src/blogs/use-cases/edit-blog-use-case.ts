d2e2be2e0afaf8eea6ec43f0039faee8613d92ef
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-10 15:46:50 +0200

    refactoring to useCases & new endpoints

--- src/blogs/use-cases/edit-blog-use-case.ts
+++ src/blogs/use-cases/edit-blog-use-case.ts
@@ -1,17 +1,26 @@
 import { BlogsRepository } from '../blogs.repository';
 import { BlogInputModel } from '../dto/blogInputModel';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { BlogsService } from '../blogs.service';
 
 export class EditBlogCommand {
-  constructor(public blogId: string, public changes: BlogInputModel) {}
+  constructor(
+    public userId: string,
+    public blogId: string,
+    public changes: BlogInputModel,
+  ) {}
 }
 
 @CommandHandler(EditBlogCommand)
 export class EditBlogUseCase implements ICommandHandler<EditBlogCommand> {
-  constructor(private blogRepository: BlogsRepository) {}
+  constructor(
+    private readonly blogRepository: BlogsRepository,
+    private readonly blogsService: BlogsService,
+  ) {}
 
   async execute(command: EditBlogCommand) {
-    const { blogId, changes } = command;
+    const { userId, blogId, changes } = command;
+    await this.blogsService.checkBlogOwner(blogId, userId);
     const editBlog = await this.blogRepository.getBlogModel(blogId);
     editBlog.blogUpdate(changes);
     return !!(await this.blogRepository.save(editBlog));
