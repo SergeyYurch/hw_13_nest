19b8abeb4ca57309034a578d254d3776f11f1c2a
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-10 15:48:30 +0200

    refactoring to useCases & new endpoints

--- src/posts/posts.repository.ts
+++ src/posts/posts.repository.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@nestjs/common';
-import { Post } from './domain/post.schema';
+import { Post, PostDocument } from './domain/post.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose';
+import { Blog, BlogDocument } from '../blogs/domain/blog.schema';
 
 @Injectable()
 export class PostsRepository {
-  async save(post): Promise<Post> {
-    return post.save();
+  constructor(
+    @InjectModel(Post.name) private PostModel: Model<PostDocument>,
+    @InjectModel(Blog.name) private BlogModel: Model<BlogDocument>,
+  ) {}
+
+  async createModel() {
+    return new this.PostModel();
+  }
+  async delete(postId: string) {
+    const result = await this.PostModel.deleteOne({
+      _id: new Types.ObjectId(postId),
+    });
+    return result.deletedCount === 1;
+  }
+
+  async findModel(postId: string) {
+    return this.PostModel.findById(postId);
+  }
+
+  async save(createdPostModel) {
+    const newPost = await createdPostModel.save();
+    return newPost?._id?.toString();
   }
 }
