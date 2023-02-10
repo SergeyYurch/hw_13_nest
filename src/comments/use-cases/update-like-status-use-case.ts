c9056ab23a7b39e5f96a3d9229fd0f4744b852cb
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-10 15:53:09 +0200

    refactoring to useCases & new endpoints

---
+++ src/comments/use-cases/update-like-status-use-case.ts
@@ -0,0 +1,31 @@
+import { UsersQueryRepository } from '../../users/users.query.repository';
+import { CommentsRepository } from '../comments.repository';
+import { LikeStatusType } from '../../common/inputModels/likeInputModel';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+
+export class UpdateLikeStatusCommand {
+  constructor(
+    public commentId: string,
+    public userId: string,
+    public likeStatus: LikeStatusType,
+  ) {}
+}
+
+@CommandHandler(UpdateLikeStatusCommand)
+export class UpdateLikeStatusUseCase
+  implements ICommandHandler<UpdateLikeStatusCommand>
+{
+  constructor(
+    protected usersQueryRepository: UsersQueryRepository,
+    protected commentsRepository: CommentsRepository,
+  ) {}
+
+  async execute(command: UpdateLikeStatusCommand) {
+    const { userId, likeStatus, commentId } = command;
+    const commentModel = await this.commentsRepository.getCommentModel(
+      commentId,
+    );
+    commentModel.updateLikeStatus(userId, likeStatus);
+    await this.commentsRepository.save(commentModel);
+  }
+}
