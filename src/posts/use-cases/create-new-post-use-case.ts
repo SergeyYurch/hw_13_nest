ef45e7cccc4e9832f06810ba46cd011ae844dbbe
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-10 15:46:17 +0200

    refactoring to useCases & new endpoints

---
+++ src/posts/use-cases/create-new-post-use-case.ts
@@ -0,0 +1,41 @@
+import { PostsRepository } from '../posts.repository';
+import { PostViewModel } from '../view-models/postViewModel';
+import { BlogsQueryRepository } from '../../blogs/blogs.query.repository';
+import { CommandHandler } from '@nestjs/cqrs';
+import { BlogsService } from '../../blogs/blogs.service';
+import { PosCreateDto } from '../dto/posCreateDto';
+import { BlogPostInputModel } from '../../blogs/dto/blogPostInputModel';
+
+export class CreateNewPostCommand {
+  constructor(
+    public userId: string,
+    public blogId: string,
+    public postInputModel: BlogPostInputModel,
+  ) {}
+}
+
+@CommandHandler(CreateNewPostCommand)
+export class CreateNewPostUseCase {
+  constructor(
+    private postRepository: PostsRepository,
+    private blogQueryRepository: BlogsQueryRepository,
+    private blogsService: BlogsService,
+  ) {}
+
+  async execute(command: CreateNewPostCommand): Promise<PostViewModel | null> {
+    const { userId, blogId, postInputModel } = command;
+    const { shortDescription, content, title } = postInputModel;
+    await this.blogsService.checkBlogOwner(blogId, userId);
+    const createdPost = await this.postRepository.createModel();
+    const blog = await this.blogQueryRepository.getBlogById(blogId);
+    const postDto: PosCreateDto = {
+      title,
+      shortDescription,
+      content,
+      blogId,
+      blogName: blog.name,
+    };
+    await createdPost.initial(postDto);
+    return await this.postRepository.save(createdPost);
+  }
+}
