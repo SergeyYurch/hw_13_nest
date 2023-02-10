77ee3dee778ce36da88849ac6e4afd146e03ecf0
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-10 15:51:56 +0200

    refactoring to useCases & new endpoints

---
+++ src/auth/application/use-cases/registration-confirmation-use-case.ts
@@ -0,0 +1,37 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { UsersRepository } from '../../../users/users.repository';
+import { BadRequestException } from '@nestjs/common';
+import { EMAIL_CONFIRMATION_MESSAGE } from '../../auth.constant';
+export class RegistrationConfirmationCommand {
+  constructor(public code: string) {}
+}
+@CommandHandler(RegistrationConfirmationCommand)
+export class RegistrationConfirmationUseCase
+  implements ICommandHandler<RegistrationConfirmationCommand>
+{
+  constructor(private userRepository: UsersRepository) {}
+
+  async execute(command: RegistrationConfirmationCommand) {
+    const { code } = command;
+    const userModel = await this.userRepository.findUserByEmailConfirmationCode(
+      code,
+    );
+    if (!userModel) {
+      throw new BadRequestException([
+        { message: EMAIL_CONFIRMATION_MESSAGE, field: 'code' },
+      ]);
+    }
+    if (userModel.emailConfirmation.expirationDate < new Date()) {
+      throw new BadRequestException([
+        { message: EMAIL_CONFIRMATION_MESSAGE, field: 'code' },
+      ]);
+    }
+    if (userModel.emailConfirmation.isConfirmed) {
+      throw new BadRequestException([
+        { message: EMAIL_CONFIRMATION_MESSAGE, field: 'code' },
+      ]);
+    }
+    userModel.confirmEmail();
+    return !!(await this.userRepository.save(userModel));
+  }
+}
