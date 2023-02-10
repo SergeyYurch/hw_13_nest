4a4fab9b8be4f8500b4ffcea6fbd3bd49bc3499c
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-10 15:42:15 +0200

    refactoring to useCases & new endpoints

--- src/auth/application/auth.service.ts
+++ src/auth/application/auth.service.ts
@@ -1,25 +1,14 @@
-import {
-  BadRequestException,
-  Injectable,
-  UnauthorizedException,
-} from '@nestjs/common';
-import {
-  EMAIL_CONFIRMATION_MESSAGE,
-  EMAIL_RESENDING_MESSAGE,
-  PASSWORD_RECOVERY_CODE_MESSAGE,
-  PASSWORD_RECOVERY_MESSAGE,
-  UNAUTHORIZED_MESSAGE,
-} from '../auth.constant';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UNAUTHORIZED_MESSAGE } from '../auth.constant';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
-import { Model, Types } from 'mongoose';
+import { Model } from 'mongoose';
 import { UsersRepository } from '../../users/users.repository';
 import { JwtPayloadType } from '../types/jwt-payload.type';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from '../../users/domain/user.schema';
 import { UsersService } from '../../users/users.service';
-import { UserInputModel } from '../../users/dto/userInputModel';
-import { MailService } from '../../common/mail.service/mail.service';
+import { Response } from 'express';
 
 @Injectable()
 export class AuthService {
@@ -29,12 +18,16 @@ export class AuthService {
     private usersService: UsersService,
     private readonly jwtService: JwtService,
     private configService: ConfigService,
-    private readonly mailService: MailService,
   ) {}
 
   async validateUser(loginOrEmail: string, password: string) {
-    const user = await this.findUserByLoginOrEmail(loginOrEmail);
-    if (!user || !(await user.validateCredentials(password))) {
+    const user = await this.userRepository.findUserByLoginOrEmail(loginOrEmail);
+    const passwordSalt = user.accountData.passwordSalt;
+    const passwordHash = await this.usersService.getPasswordHash(
+      password,
+      passwordSalt,
+    );
+    if (!user || !(await user.validateCredentials(passwordHash))) {
       throw new UnauthorizedException([
         { message: UNAUTHORIZED_MESSAGE, field: 'loginOrEmail' },
       ]);
@@ -52,15 +45,6 @@ export class AuthService {
     }
   }
 
-  async findUserByLoginOrEmail(loginOrEmail: string) {
-    return await this.UserModel.findOne({
-      $or: [
-        { 'accountData.email': loginOrEmail },
-        { 'accountData.login': loginOrEmail },
-      ],
-    }).exec();
-  }
-
   async validateUsersDeviceSession(jwtPayload: JwtPayloadType) {
     const user = await this.UserModel.findById(jwtPayload.userId);
     if (!user) {
@@ -75,19 +59,12 @@ export class AuthService {
     }
   }
 
-  async signIn(
-    loginOrEmail: string,
-    password: string,
-    ip = '0.0.0.0',
-    title = 'no data',
-  ) {
-    const user = await this.validateUser(loginOrEmail, password);
-    const deviceId = new Types.ObjectId().toString();
-    const { accessToken, refreshToken, expiresDate, lastActiveDate } =
-      await this.getTokens(user._id.toString(), deviceId);
-    await user.signIn(deviceId, ip, title, expiresDate, lastActiveDate);
-    await this.userRepository.save(user);
-    return { accessToken, refreshToken, expiresDate };
+  getCookiesWithToken(res: Response, refreshToken: string, expiresDate) {
+    res.cookie('refreshToken', refreshToken, {
+      expires: new Date(expiresDate),
+      // secure: true,
+      httpOnly: true,
+    });
   }
 
   async getTokens(userId: string, deviceId: string) {
@@ -120,108 +97,5 @@ export class AuthService {
       expiresDate: jwtPayload.exp * 1000,
       lastActiveDate: jwtPayload.iat * 1000,
     };
-  }
-
-  async logout(userId: string, deviceId: string) {
-    const user = await this.UserModel.findById(userId);
-    user.logout(deviceId);
-    await this.userRepository.save(user);
-  }
-
-  async refreshTokens(userId: string, deviceId: string) {
-    const { accessToken, refreshToken, expiresDate, lastActiveDate } =
-      await this.getTokens(userId, deviceId);
-    const user = await this.UserModel.findById(userId);
-    user.refreshTokens(deviceId, expiresDate, lastActiveDate);
-    await this.userRepository.save(user);
-    return { accessToken, refreshToken, expiresDate };
-  }
-
-  // async registration(userDto: UserInputModel) {
-  //   return await this.usersService.createNewUser(userDto);
-  // }
-
-  async registrationConfirmation(code: string) {
-    const user = await this.UserModel.findOne({
-      'emailConfirmation.confirmationCode': code,
-    }).exec();
-    if (!user) {
-      throw new BadRequestException([
-        { message: EMAIL_CONFIRMATION_MESSAGE, field: 'code' },
-      ]);
-    }
-    if (user.emailConfirmation.expirationDate < new Date()) {
-      throw new BadRequestException([
-        { message: EMAIL_CONFIRMATION_MESSAGE, field: 'code' },
-      ]);
-    }
-    if (user.emailConfirmation.isConfirmed) {
-      throw new BadRequestException([
-        { message: EMAIL_CONFIRMATION_MESSAGE, field: 'code' },
-      ]);
-    }
-    user.confirmEmail();
-    await this.userRepository.save(user);
-  }
-
-  async registrationEmailResending(email: string) {
-    const user = await this.findUserByLoginOrEmail(email);
-    if (!user) {
-      throw new BadRequestException([
-        { message: EMAIL_RESENDING_MESSAGE, field: 'email' },
-      ]);
-    }
-    if (user.emailConfirmation.isConfirmed) {
-      throw new BadRequestException([
-        { message: EMAIL_RESENDING_MESSAGE, field: 'email' },
-      ]);
-    }
-    user.generateNewEmailConfirmationCode();
-    await this.mailService.sendConfirmationEmail(
-      user.accountData.email,
-      user.emailConfirmation.confirmationCode,
-    );
-    await this.userRepository.save(user);
-  }
-
-  async passwordRecovery(email: string) {
-    const user = await this.findUserByLoginOrEmail(email);
-    if (!user) {
-      throw new BadRequestException([
-        { message: PASSWORD_RECOVERY_MESSAGE, field: 'email' },
-      ]);
-    }
-    user.generateNewPasswordRecoveryCode();
-    await this.userRepository.save(user);
-    const resultSendEmail = await this.mailService.sendPasswordRecoveryEmail(
-      user.accountData.email,
-      user.passwordRecoveryInformation.recoveryCode,
-    );
-    if (!resultSendEmail) console.log('email did not send');
-    return resultSendEmail;
-  }
-
-  async setNewPassword(recoveryCode: string, newPassword: string) {
-    const user = await this.UserModel.findOne({
-      'passwordRecoveryInformation.recoveryCode': recoveryCode,
-    }).exec();
-    if (!user) {
-      throw new BadRequestException([
-        { message: PASSWORD_RECOVERY_CODE_MESSAGE, field: 'recoveryCode' },
-      ]);
-    }
-    if (user.passwordRecoveryInformation.expirationDate < new Date()) {
-      throw new BadRequestException([
-        { message: PASSWORD_RECOVERY_CODE_MESSAGE, field: 'recoveryCode' },
-      ]);
-    }
-    if (user.passwordRecoveryInformation.recoveryCode !== recoveryCode) {
-      throw new BadRequestException([
-        { message: PASSWORD_RECOVERY_CODE_MESSAGE, field: 'recoveryCode' },
-      ]);
-    }
-    await user.setNewPassword(newPassword);
-    await this.userRepository.save(user);
-    return;
   }
 }
