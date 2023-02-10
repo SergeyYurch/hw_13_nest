2e5542d3ec9af66c438e50716c29efe9606150aa
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-10 15:43:52 +0200

    refactoring to useCases & new endpoints

--- src/comments/comments.repository.ts
+++ src/comments/comments.repository.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@nestjs/common';
-import { CommentDocument } from './domain/comment.schema';
+import { Comment, CommentDocument } from './domain/comment.schema';
+import { Model, Types } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class CommentsRepository {
+  constructor(
+    @InjectModel(Comment.name) private CommentModel: Model<CommentDocument>,
+  ) {}
+
+  async getCommentModel(id: string) {
+    return this.CommentModel.findById(id);
+  }
+
+  async createCommentModel() {
+    return new this.CommentModel();
+  }
+
+  async deleteComment(commentId: string) {
+    const result = await this.CommentModel.deleteOne({
+      _id: new Types.ObjectId(commentId),
+    });
+    return result.deletedCount === 1;
+  }
+
   async save(createdComment: CommentDocument) {
-    return createdComment.save();
+    const newComment = await createdComment.save();
+    return newComment?._id?.toString();
   }
 }
