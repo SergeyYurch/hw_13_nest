d0d5a12a116e414412ec44c997fd7a937ae5b379
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-10 15:42:56 +0200

    refactoring to useCases & new endpoints

--- src/blogs/blogger-blogs.controller.ts
+++ src/blogs/blogger-blogs.controller.ts
@@ -15,15 +15,11 @@ import {
 import { BlogsService } from './blogs.service';
 import { BlogInputModel } from './dto/blogInputModel';
 import { castQueryParams } from '../common/helpers/helpers';
-import { PaginatorInputType } from '../common/inputModels/paginatorInputType';
 import { PostViewModel } from '../posts/view-models/postViewModel';
-import { PaginatorView } from '../common/view-models/paginatorView';
 import { BlogPostInputModel } from './dto/blogPostInputModel';
 import { PostsService } from '../posts/posts.service';
-import { PostInputModel } from '../posts/dto/postInputModel';
 import { ValidateObjectIdTypePipe } from '../common/pipes/validateObjectIdType.pipe';
 import { CurrentUserJwtInfo } from '../common/decorators/current-user.param.decorator';
-import { JwtPayloadType } from '../auth/types/jwt-payload.type';
 import { BlogsQueryRepository } from './blogs.query.repository';
 import { PostsQueryRepository } from '../posts/posts.query.repository';
 import { AccessTokenGuard } from '../auth/guards/access-token.guard';
@@ -31,6 +27,9 @@ import { CommandBus } from '@nestjs/cqrs';
 import { CreateNewBlogCommand } from './use-cases/create-new-blog-use-case';
 import { EditBlogCommand } from './use-cases/edit-blog-use-case';
 import { DeleteBlogCommand } from './use-cases/delete-blog-use-case';
+import { CreateNewPostCommand } from '../posts/use-cases/create-new-post-use-case';
+import { EditPostCommand } from '../posts/use-cases/edit-post-use-case';
+import { DeletePostCommand } from '../posts/use-cases/delete-post-use-case';
 
 @UseGuards(AccessTokenGuard)
 @Controller('blogger/blogs')
@@ -42,6 +41,53 @@ export class BloggerBlogsController {
     private postsQueryRepository: PostsQueryRepository,
     private commandBus: CommandBus,
   ) {}
+
+  @Put(':blogId')
+  @HttpCode(204)
+  async editBlog(
+    @Param('blogId') blogId: string,
+    @Body() changes: BlogInputModel,
+    @CurrentUserJwtInfo() userId: string,
+  ) {
+    if (!(await this.blogsQueryRepository.checkBlogId(blogId))) {
+      throw new NotFoundException('Invalid blogId');
+    }
+    const result = await this.commandBus.execute(
+      new EditBlogCommand(userId, blogId, changes),
+    );
+    if (!result) {
+      throw new InternalServerErrorException('Blog not changed');
+    }
+  }
+
+  @Delete(':blogId')
+  @HttpCode(204)
+  async deleteBlog(
+    @CurrentUserJwtInfo() userId: string,
+    @Param('blogId', ValidateObjectIdTypePipe) blogId: string,
+  ) {
+    if (!(await this.blogsQueryRepository.checkBlogId(blogId))) {
+      throw new NotFoundException('Invalid blogId');
+    }
+
+    const result = await this.commandBus.execute(
+      new DeleteBlogCommand(userId, blogId),
+    );
+    if (!result) {
+      throw new InternalServerErrorException('Blog not deleted');
+    }
+  }
+
+  @Post()
+  async createBlog(
+    @Body() blog: BlogInputModel,
+    @CurrentUserJwtInfo() userId: string,
+  ) {
+    const blogId = await this.commandBus.execute(
+      new CreateNewBlogCommand(blog, userId),
+    );
+    return this.blogsQueryRepository.getBlogById(blogId);
+  }
 
   @Get()
   async getBlogs(
@@ -57,90 +103,61 @@ export class BloggerBlogsController {
     );
   }
 
-  // @Roles(Role.Blogger)
-  @Post()
-  async createBlog(
-    @Body() blog: BlogInputModel,
-    @CurrentUserJwtInfo() userId: string,
-  ) {
-    return await this.commandBus.execute(
-      new CreateNewBlogCommand(blog, userId),
-    );
-  }
-
-  @Put(':blogId')
-  @HttpCode(204)
-  async editBlog(
-    @Param('blogId') blogId: string,
-    @Body() changes: BlogInputModel,
-    @CurrentUserJwtInfo() userId: string,
-  ) {
-    if (!(await this.blogsQueryRepository.checkBlogId(blogId))) {
-      throw new NotFoundException('Invalid blogId');
-    }
-    await this.blogsService.checkBlogOwner(blogId, userId);
-    await this.commandBus.execute(new EditBlogCommand(blogId, changes));
-  }
-
-  @Delete(':blogId')
-  @HttpCode(204)
-  async deleteBlog(
-    @CurrentUserJwtInfo() userId: string,
-    @Param('blogId', ValidateObjectIdTypePipe) blogId: string,
-  ) {
-    if (!(await this.blogsQueryRepository.checkBlogId(blogId))) {
-      throw new NotFoundException('Invalid blogId');
-    }
-    await this.blogsService.checkBlogOwner(blogId, userId);
-
-    const result = await this.commandBus.execute(new DeleteBlogCommand(blogId));
-    if (!result) {
-      throw new InternalServerErrorException('Blog not deleted');
-    }
-    return;
-  }
-
-  @Get(':blogId/posts')
-  async getPostsForBlog(
-    @Param('blogId', ValidateObjectIdTypePipe) blogId: string,
-    @Query() query: PaginatorInputType,
-    @CurrentUserJwtInfo() userInfo: JwtPayloadType,
-  ): Promise<PaginatorView<PostViewModel>> {
-    if (!(await this.blogsQueryRepository.checkBlogId(blogId))) {
-      throw new NotFoundException('Invalid blogId');
-    }
-    const userId = userInfo.userId;
-    const paginatorParams = castQueryParams(query);
-    return await this.postsQueryRepository.findPosts(
-      paginatorParams,
-      blogId,
-      userId,
-    );
-  }
-
   @Post(':blogId/posts')
   async createPostForBlog(
+    @CurrentUserJwtInfo() userId: string,
     @Param('blogId', ValidateObjectIdTypePipe) blogId: string,
-    @Body() blogPostDto: BlogPostInputModel,
+    @Body() blogPostInputModel: BlogPostInputModel,
   ): Promise<PostViewModel> {
     if (!(await this.blogsQueryRepository.checkBlogId(blogId))) {
       throw new NotFoundException('Invalid blogId');
     }
-    const createdPost: PostInputModel = {
-      title: blogPostDto.title,
-      shortDescription: blogPostDto.shortDescription,
-      content: blogPostDto.content,
-      blogId,
-    };
-    return this.postService.createNewPost(createdPost);
+    const postId = await this.commandBus.execute(
+      new CreateNewPostCommand(userId, blogId, blogPostInputModel),
+    );
+    return await this.postsQueryRepository.getPostById(postId);
   }
 
-  @Get(':blogId')
-  async getBlog(@Param('blogId', ValidateObjectIdTypePipe) blogId: string) {
-    const blog = await this.blogsQueryRepository.getBlogById(blogId);
-    if (!blog) {
+  @Put(':blogId/posts/:postId')
+  @HttpCode(204)
+  async editPost(
+    @CurrentUserJwtInfo() userId: string,
+    @Param('blogId', ValidateObjectIdTypePipe) blogId: string,
+    @Param('postId', ValidateObjectIdTypePipe) postId: string,
+    @Body() postChanges: BlogPostInputModel,
+  ) {
+    if (!(await this.blogsQueryRepository.checkBlogId(blogId))) {
       throw new NotFoundException('Invalid blogId');
     }
-    return blog;
+    if (!(await this.postsQueryRepository.checkPostId(postId))) {
+      throw new NotFoundException('Invalid postID');
+    }
+    const result = await this.commandBus.execute(
+      new EditPostCommand(userId, blogId, postId, postChanges),
+    );
+    if (!result) {
+      throw new InternalServerErrorException('Post not changed');
+    }
+  }
+
+  @Delete(':blogId/posts/:postId')
+  @HttpCode(204)
+  async deletePost(
+    @CurrentUserJwtInfo() userId: string,
+    @Param('blogId', ValidateObjectIdTypePipe) blogId: string,
+    @Param('postId', ValidateObjectIdTypePipe) postId: string,
+  ) {
+    if (!(await this.blogsQueryRepository.checkBlogId(blogId))) {
+      throw new NotFoundException('Invalid blogId');
+    }
+    if (!(await this.postsQueryRepository.checkPostId(postId))) {
+      throw new NotFoundException('Invalid postID');
+    }
+    const result = await this.commandBus.execute(
+      new DeletePostCommand(userId, blogId, postId),
+    );
+    if (!result) {
+      throw new InternalServerErrorException('Post not changed');
+    }
   }
 }
