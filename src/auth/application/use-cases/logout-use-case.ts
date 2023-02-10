eb76cc82349239858e9dd5028c75b73d8d31cadc
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-10 15:47:53 +0200

    refactoring to useCases & new endpoints

---
+++ src/auth/application/use-cases/logout-use-case.ts
@@ -0,0 +1,17 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { UsersRepository } from '../../../users/users.repository';
+
+export class LogoutCommand {
+  constructor(public userId: string, public deviceId: string) {}
+}
+@CommandHandler(LogoutCommand)
+export class LogoutUseCase implements ICommandHandler<LogoutCommand> {
+  constructor(private userRepository: UsersRepository) {}
+
+  async execute(command: LogoutCommand) {
+    const { userId, deviceId } = command;
+    const user = await this.userRepository.getUserModel(userId);
+    user.logout(deviceId);
+    return !!(await this.userRepository.save(user));
+  }
+}
