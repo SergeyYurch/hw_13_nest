ec979ac2843527b6246ac0976aa06a5a3a37aeb1
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-10 15:43:37 +0200

    refactoring to useCases & new endpoints

--- src/blogs/blogs.service.ts
+++ src/blogs/blogs.service.ts
@@ -1,10 +1,4 @@
-import { Model } from 'mongoose';
 import { ForbiddenException, Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Blog, BlogDocument } from './domain/blog.schema';
-import { BlogInputModel } from './dto/blogInputModel';
-import { BlogViewModel } from './view-models/blogViewModel';
-import { BlogsRepository } from './blogs.repository';
 import { BlogsQueryRepository } from './blogs.query.repository';
 //
 // @Injectable
@@ -21,34 +15,11 @@ import { BlogsQueryRepository } from './blogs.query.repository';
 
 @Injectable()
 export class BlogsService {
-  constructor(
-    @InjectModel(Blog.name) private BlogModel: Model<BlogDocument>,
-    private blogRepository: BlogsRepository,
-    private blogsQueryRepository: BlogsQueryRepository,
-  ) {}
-
-  // async createNewBlog(inputBlogDto: BlogInputModel): Promise<BlogViewModel> {
-  //   const createdBlog = await this.blogRepository.createBlogModel();
-  //   createdBlog.initial(inputBlogDto);
-  //   // const factory = new factory();
-  //   // const blog = factory.createBlog(dto);
-  //   const newBlog = await this.blogRepository.save(createdBlog);
-  //   return this.blogsQueryRepository.getBlogById(newBlog._id.toString());
-  // }
-
-  // async editBlog(blogId: string, changes: BlogInputModel): Promise<boolean> {
-  //   const editBlog = await this.blogRepository.getBlogModel(blogId);
-  //   editBlog.blogUpdate(changes);
-  //   return !!(await this.blogRepository.save(editBlog));
-  // }
-
-  // async deleteBlog(blogId: string): Promise<boolean> {
-  //   return this.blogRepository.deleteBlog(blogId);
-  // }
+  constructor(private blogsQueryRepository: BlogsQueryRepository) {}
 
   async checkBlogOwner(blogId: string, userId: string) {
     const owner = await this.blogsQueryRepository.getBlogOwner(blogId);
-    if (owner.userId === userId) {
+    if (owner.userId !== userId) {
       throw new ForbiddenException('Forbidden');
     }
   }
