eec3d6e9a1312925a2285cfad371403b6c1f20b1
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-12 15:47:29 +0200

    fix error - ban users

--- src/users/use-cases/ban-user-use-case.ts
+++ src/users/use-cases/ban-user-use-case.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { UsersRepository } from '../users.repository';
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { CommandBus, CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { BanUserInputModel } from '../dto/ban -user-input-model.dto';
+import { BanCommentCommand } from '../../comments/use-cases/ban-comment-use-case';
+import { BanCommentLikesCommand } from '../../comments/use-cases/ban-comment-likes-use-case';
+import { BanPostLikesCommand } from '../../posts/use-cases/ban-post-likes-use-case';
+import { BanPostsCommand } from '../../posts/use-cases/ban-posts-use-case';
 
 export class BanUserCommand {
   constructor(
@@ -13,10 +17,17 @@ export class BanUserCommand {
 @Injectable()
 @CommandHandler(BanUserCommand)
 export class BanUserUseCase implements ICommandHandler<BanUserCommand> {
-  constructor(private readonly usersRepository: UsersRepository) {}
+  constructor(
+    private readonly usersRepository: UsersRepository,
+    private commandBus: CommandBus,
+  ) {}
   async execute(command: BanUserCommand) {
     const { userId, banUserInputModel } = command;
     const { isBanned, banReason } = banUserInputModel;
+    await this.commandBus.execute(new BanCommentCommand(userId, isBanned));
+    await this.commandBus.execute(new BanCommentLikesCommand(userId, isBanned));
+    await this.commandBus.execute(new BanPostsCommand(userId, isBanned));
+    await this.commandBus.execute(new BanPostLikesCommand(userId, isBanned));
     const userModel = await this.usersRepository.getUserModel(userId);
     await userModel.ban(isBanned, banReason, 'saId');
     return await this.usersRepository.save(userModel);
