cf8b3c07b7002ec65bf1648d3673cbe6b1e89d26
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-19 22:41:37 +0200

    fix error / refactor app

--- src/users/providers/use-cases/registration-user-use-case.ts
+++ src/users/providers/use-cases/registration-user-use-case.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { UsersQueryRepository } from '../users.query.repository';
-import { MailService } from '../../common/mail.service/mail.service';
-import { UserInputModel } from '../dto/userInputModel';
+import { MailService } from '../../../common/mail.service/mail.service';
+import { UserInputModel } from '../../dto/input-models/user-input-model';
 import { CommandBus, CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { CreateNewUserCommand } from './create-new-user-use-case';
+import { UserCreatDto } from '../../dto/user-creat.dto';
+import { UsersService } from '../users.service';
+import { UsersRepository } from '../users.repository';
 
 export class RegistrationUserCommand {
   constructor(public userInputModel: UserInputModel) {}
@@ -17,16 +20,31 @@ export class RegistrationUserUseCase
   constructor(
     private commandBus: CommandBus,
     private readonly usersQueryRepository: UsersQueryRepository,
+    private readonly usersRepository: UsersRepository,
+    private readonly usersService: UsersService,
     private readonly mailService: MailService,
   ) {}
   async execute(command: RegistrationUserCommand) {
     const { userInputModel } = command;
-    const userId = await this.commandBus.execute(
-      new CreateNewUserCommand(userInputModel),
+    const { login, email, password } = userInputModel;
+    const passwordSalt = await this.usersService.getPasswordSalt();
+    const passwordHash = await this.usersService.getPasswordHash(
+      password,
+      passwordSalt,
     );
+    const user: UserCreatDto = {
+      login,
+      email,
+      passwordSalt,
+      passwordHash,
+      isConfirmed: false,
+    };
+    const userModel = await this.usersRepository.createUserModel();
+    await userModel.initialize(user);
+    const userId = await this.usersRepository.save(userModel);
 
     if (!userId) return null;
-    const { email, confirmationCode } =
+    const { confirmationCode } =
       await this.usersQueryRepository.getEmailConfirmationData(userId);
     await this.mailService.sendConfirmationEmail(email, confirmationCode);
     return this.usersQueryRepository.getUserById(userId);
