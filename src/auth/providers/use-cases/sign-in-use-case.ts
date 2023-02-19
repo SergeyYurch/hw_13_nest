a7df65eb4e0e469b70fd08b3d8903bace3def94c
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-19 22:42:23 +0200

    fix error / refactor app

--- src/auth/providers/use-cases/sign-in-use-case.ts
+++ src/auth/providers/use-cases/sign-in-use-case.ts
@@ -1,5 +1,5 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
-import { UsersRepository } from '../../../users/users.repository';
+import { UsersRepository } from '../../../users/providers/users.repository';
 import { Types } from 'mongoose';
 import { AuthService } from '../auth.service';
 
