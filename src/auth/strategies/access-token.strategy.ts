df25ec172dec1a7836213751da91ca9f596b65e6
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-20 08:38:14 +0200

    fix error / refactor app

--- src/auth/strategies/access-token.strategy.ts
+++ src/auth/strategies/access-token.strategy.ts
@@ -4,18 +4,16 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { AuthService } from '../providers/auth.service';
 
 @Injectable()
-export class AccessTokenStrategy extends PassportStrategy(
-  Strategy,
-  'jwt-access',
-) {
+export class AccessTokenStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.JWT_ACCESS_SECRET,
+      secretOrKey: process.env.JWT_ACCESS_SECRET || 11,
     });
   }
 
   async validate(payload: any) {
+    console.log('AccessTokenStrategy is ok');
     //await this.authService.validateSigInStatus(payload.userId);
     return payload;
   }
