d0c3a38494cd40f57d2d0abfd958a92aac6be30e
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-24 13:46:34 +0200

    fix error / add functional of HW16

--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -9,6 +9,7 @@ import { Blog, BlogDocument } from '../../blogs/domain/blog.schema';
 import { LikeStatusType } from '../../common/dto/input-models/like.input.model';
 import { PostsQueryRepository } from './posts.query.repository';
 import { UsersQueryRepository } from '../../users/providers/users.query.repository';
+import { BlogsQueryRepository } from '../../blogs/providers/blogs.query.repository';
 
 @Injectable()
 export class PostsService {
@@ -17,47 +18,13 @@ export class PostsService {
     @InjectModel(Blog.name) private BlogModel: Model<BlogDocument>,
     private postRepository: PostsRepository,
     private postsQueryRepository: PostsQueryRepository,
-    private usersQueryRepository: UsersQueryRepository,
+    private blogsQueryRepository: BlogsQueryRepository,
   ) {}
 
-  async createNewPost(postDto: PostInputModel): Promise<PostViewModel | null> {
-    const createdPost = new this.PostModel(postDto);
-    const blogInDb = await this.BlogModel.findById(postDto.blogId);
-    createdPost.blogName = blogInDb.name;
-    createdPost.createdAt = new Date();
-    const result = await this.postRepository.save(createdPost);
-    if (!postDto) return null;
-    return this.postsQueryRepository.getPostViewModel(result);
-  }
-
-  async editPostById(
-    postId: string,
-    postChanges: PostInputModel,
-  ): Promise<boolean> {
-    const postInDd = await this.PostModel.findById(postId);
-    postInDd.title = postChanges.title;
-    postInDd.shortDescription = postChanges.shortDescription;
-    postInDd.content = postChanges.content;
-    postInDd.blogId = postChanges.blogId;
-    const result = await this.postRepository.save(postInDd);
-    return !!result;
-  }
-
-  async deletePostById(postId: string): Promise<boolean> {
-    const result = await this.PostModel.deleteOne({
-      _id: new Types.ObjectId(postId),
-    });
-    return result.deletedCount === 1;
-  }
-
-  async updatePostLikeStatus(
-    postId: string,
-    userId: string,
-    likeStatus: LikeStatusType,
-  ) {
-    const post = await this.PostModel.findById(postId);
-    const { login } = await this.usersQueryRepository.getUserById(userId);
-    post.updateLikeStatus(userId, login, likeStatus);
-    await this.postRepository.save(post);
+  async accessCheck(userId, postId: string) {
+    const blogId = (await this.postsQueryRepository.getPostById(postId)).blogId;
+    if (await this.blogsQueryRepository.isUserBanned(userId, blogId))
+      return 'forbidden';
+    return 'allowed';
   }
 }
