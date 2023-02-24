0dae5467d944d3d965497a6f6fe45202cd6f68ca
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-24 13:46:04 +0200

    fix error / add functional of HW16

--- src/blogs/providers/use-cases/edit-blog-use-case.ts
+++ src/blogs/providers/use-cases/edit-blog-use-case.ts
@@ -2,12 +2,13 @@ import { BlogsRepository } from '../blogs.repository';
 import { BlogInputModel } from '../../dto/input-models/blog.input.model';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { BlogsService } from '../blogs.service';
+import { BlogEditDto } from '../../dto/blog-edit.dto';
 
 export class EditBlogCommand {
   constructor(
     public userId: string,
     public blogId: string,
-    public changes: BlogInputModel,
+    public inputDate: BlogInputModel,
   ) {}
 }
 
@@ -19,7 +20,12 @@ export class EditBlogUseCase implements ICommandHandler<EditBlogCommand> {
   ) {}
 
   async execute(command: EditBlogCommand) {
-    const { userId, blogId, changes } = command;
+    const { userId, blogId, inputDate } = command;
+    const changes: BlogEditDto = {
+      name: inputDate.name,
+      websiteUrl: inputDate.websiteUrl,
+      description: inputDate.description,
+    };
     await this.blogsService.checkBlogOwner(blogId, userId);
     const editBlog = await this.blogRepository.getBlogModel(blogId);
     editBlog.blogUpdate(changes);
