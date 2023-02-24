cd1b19a77227b7bfd04817c91ae62308e035fabe
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-24 13:46:51 +0200

    fix error / add functional of HW16

--- test/sa-blogs-contoller.e2e-spec.ts
+++ test/sa-blogs-contoller.e2e-spec.ts
@@ -152,8 +152,7 @@ describe('UsersController (e2e)', () => {
     accessTokenUser3 = sigInUser3.body.accessToken;
     app.getHttpServer().close();
   });
-  it('POST:[HOST]/sa/blogs: should return code 201 and newBlog for correct input data', async () => {
-    //create blog without owner for tests
+  it('POST:[HOST]/sa/blogs: create blog1 without owner for tests', async () => {
     const newBlog1 = await request(app.getHttpServer())
       .post('/sa/blogs')
       .auth('admin', 'qwerty', { type: 'basic' })
@@ -180,7 +179,7 @@ describe('UsersController (e2e)', () => {
       .get(`/sa/blogs`)
       .auth('admin', 'qwerty', { type: 'basic' });
     const blog1InDb = getBlogsResult.body.items[0];
-    expect(blog1InDb.blogOwnerInfo).toBeUndefined();
+    expect(blog1InDb.blogOwnerInfo.userId).toBeNull();
   });
 
   // PUT: [HOST]sa/blogs/{id}/bind-with-user/{userId} Bind Blog with user
@@ -210,13 +209,14 @@ describe('UsersController (e2e)', () => {
   });
 
   //GET: [HOST]/sa/blogs
-  it('DELETE:[HOST]/blogger/blogs/{:id}: should return code 204 for correct request, and should return 404 for GET by id', async () => {
+  //preparation
+  it('DELETE:[HOST]/blogger/blogs/{:id}: delete blog1', async () => {
     await request(app.getHttpServer())
       .delete(`/blogger/blogs/${blog1Id}`)
       .auth(accessTokenUser1, { type: 'bearer' })
       .expect(204);
   });
-  it('POST:[HOST]/blogger/blogs: should return code 201 and newBlog for correct input data', async () => {
+  it('POST:[HOST]/blogger/blogs: create user1/blog1, user2/blog2, user3/blog3', async () => {
     const newBlog1 = await request(app.getHttpServer())
       .post('/blogger/blogs')
       .auth(accessTokenUser1, { type: 'bearer' })
@@ -227,7 +227,7 @@ describe('UsersController (e2e)', () => {
 
     const newBlog2 = await request(app.getHttpServer())
       .post('/blogger/blogs')
-      .auth(accessTokenUser1, { type: 'bearer' })
+      .auth(accessTokenUser2, { type: 'bearer' })
       .send(blog2)
       .expect(201);
     blog2View = newBlog2.body;
@@ -235,11 +235,52 @@ describe('UsersController (e2e)', () => {
 
     const newBlog3 = await request(app.getHttpServer())
       .post('/blogger/blogs')
-      .auth(accessTokenUser1, { type: 'bearer' })
+      .auth(accessTokenUser3, { type: 'bearer' })
       .send(blog3)
       .expect(201);
     blog3View = newBlog3.body;
     blog3Id = newBlog3.body.id;
+  });
+  it('POST:[HOST]/blogger/blogs/{:blogId}/posts: user1 create blog1/post1,  user2 create posts for blog2/post2/post3', async () => {
+    const newPost1 = await request(app.getHttpServer())
+      .post(`/blogger/blogs/${blog1Id}/posts`)
+      .auth(accessTokenUser1, { type: 'bearer' })
+      .send({
+        title: 'title-post1',
+        shortDescription: 'created user2 for blog2',
+        content: 'content1',
+      })
+      .expect(201);
+    post1Id = newPost1.body.id;
+
+    const newPost2 = await request(app.getHttpServer())
+      .post(`/blogger/blogs/${blog2Id}/posts`)
+      .auth(accessTokenUser2, { type: 'bearer' })
+      .send({
+        title: 'title-post2',
+        shortDescription: 'created user2 for blog2',
+        content: 'content2',
+      })
+      .expect(201);
+    post2Id = newPost2.body.id;
+
+    const newPost3 = await request(app.getHttpServer())
+      .post(`/blogger/blogs/${blog2Id}/posts`)
+      .auth(accessTokenUser2, { type: 'bearer' })
+      .send({
+        title: 'title-post3',
+        shortDescription: 'created user2 for blog2',
+        content: 'content3',
+      })
+      .expect(201);
+    post3Id = newPost3.body.id;
+  });
+  it('GET: [HOST]/posts should return 3 posts', async () => {
+    const getPostsResult = await request(app.getHttpServer())
+      .get(`/posts`)
+      .expect(200);
+    expect(getPostsResult.body.totalCount).toBe(3);
+    expect(getPostsResult.body.items.length).toBe(3);
   });
 
   //checking pagination
@@ -248,12 +289,109 @@ describe('UsersController (e2e)', () => {
     const getBlogsResult = await request(app.getHttpServer())
       .get(`/sa/blogs?sortDirection=asc`)
       .auth('admin', 'qwerty', { type: 'basic' });
+    expect(getBlogsResult.body.totalCount).toBe(3);
+    expect(getBlogsResult.body.items.length).toBe(3);
     expect(getBlogsResult.body.items[0].name).toBe('blog1');
+    expect(getBlogsResult.body.items[0]).toEqual({
+      id: expect.any(String),
+      name: 'blog1',
+      description: 'description1',
+      websiteUrl: 'https://youtube1.com',
+      createdAt: expect.any(String),
+      isMembership: false,
+      blogOwnerInfo: {
+        userId: user1Id,
+        userLogin: 'user1',
+      },
+      banInfo: {
+        isBanned: false,
+        banDate: null,
+      },
+    });
   });
   it('GET: [HOST]/sa/blogs/ should return code 201 and  blog2 if query has searchNameTerm = og2', async () => {
     const getBlogsResult = await request(app.getHttpServer())
       .get(`/sa/blogs?searchNameTerm=og2`)
       .auth('admin', 'qwerty', { type: 'basic' });
     expect(getBlogsResult.body.items[0]?.name).toBe('blog2');
+  });
+
+  //ban blog
+  it('PUT: [HOST]/sa/blogs/{:id}/ban: should return should return code 401 for unauthorized user', async () => {
+    await request(app.getHttpServer())
+      .put(`/sa/blogs/${blog1Id}/ban`)
+      .send({ isBanned: true })
+      .expect(401);
+  });
+  it('PUT: [HOST]/sa/blogs/{:id}/ban: should return should return code 400 if input data is incorrect', async () => {
+    const errorMessage = await request(app.getHttpServer())
+      .put(`/sa/blogs/${blog1Id}/ban`)
+      .auth('admin', 'qwerty', { type: 'basic' })
+      .send({ isBanne: true })
+      .expect(400);
+    expect(errorMessage.body).toEqual({
+      errorsMessages: [
+        {
+          message: expect.any(String),
+          field: 'isBanned',
+        },
+      ],
+    });
+  });
+  it('PUT: [HOST]/sa/blogs/{:id}/ban: should return code 204 for correct data and blog should be banned', async () => {
+    await request(app.getHttpServer())
+      .put(`/sa/blogs/${blog1Id}/ban`)
+      .send({ isBanned: true })
+      .auth('admin', 'qwerty', { type: 'basic' })
+      .expect(204);
+  });
+
+  //check allowed blogs/posts
+  it('GET: [HOST]/sa/blogs/ check if blog1 is banned', async () => {
+    const getBlogsResult = await request(app.getHttpServer())
+      .get(`/sa/blogs?searchNameTerm=blog1`)
+      .auth('admin', 'qwerty', { type: 'basic' });
+    expect(getBlogsResult.body.items[0]?.banInfo.isBanned).toBe(true);
+  });
+  it('GET: [HOST]/posts/{:id} should return 404 for post1/blog if blog1 is banned', async () => {
+    const getPostsResult = await request(app.getHttpServer())
+      .get(`/posts/${post1Id}/`)
+      .expect(404);
+  });
+  it('GET: [HOST]/posts should return code 200 and posts(unauthorized user)', async () => {
+    const getPostsResult = await request(app.getHttpServer())
+      .get(`/posts`)
+      .expect(200);
+    expect(getPostsResult.body.totalCount).toBe(2);
+    expect(getPostsResult.body.items.length).toBe(2);
+  });
+  //unban blog
+  it('PUT: [HOST]/sa/blogs/{:id}/ban: should return code 204 for correct data and blog should be unbanned', async () => {
+    await request(app.getHttpServer())
+      .put(`/sa/blogs/${blog1Id}/ban`)
+      .send({ isBanned: false })
+      .auth('admin', 'qwerty', { type: 'basic' })
+      .expect(204);
+  });
+
+  //check allowed unbanned blogs/posts
+  it('GET: [HOST]/sa/blogs/ check if blog1 is banned', async () => {
+    const getBlogsResult = await request(app.getHttpServer())
+      .get(`/sa/blogs?searchNameTerm=blog1`)
+      .auth('admin', 'qwerty', { type: 'basic' });
+    expect(getBlogsResult.body.items[0]?.banInfo.isBanned).toBe(false);
+  });
+  it('GET: [HOST]/posts/{:id} should return 404 for post1/blog if blog1 is banned', async () => {
+    const getPostsResult = await request(app.getHttpServer())
+      .get(`/posts/${post1Id}/`)
+      .expect(200);
+    expect(getPostsResult.body.title).toBe('title-post1');
+  });
+  it('GET: [HOST]/posts should return code 200 and posts(unauthorized user)', async () => {
+    const getPostsResult = await request(app.getHttpServer())
+      .get(`/posts`)
+      .expect(200);
+    expect(getPostsResult.body.totalCount).toBe(3);
+    expect(getPostsResult.body.items.length).toBe(3);
   });
 });
