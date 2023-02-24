f8b026fe2e5db506c2bdb163a8280cc6770719bf
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-24 13:45:23 +0200

    fix error / add functional of HW16

--- src/comments/providers/use-cases/create-comment-use-case.ts
+++ src/comments/providers/use-cases/create-comment-use-case.ts
@@ -1,11 +1,13 @@
 import { UsersQueryRepository } from '../../../users/providers/users.query.repository';
 import { CommentsRepository } from '../comments.repository';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { PostsQueryRepository } from '../../../posts/providers/posts.query.repository';
+import { CreatedCommentDto } from '../../dto/created-comment.dto';
 
 export class CreateCommentCommand {
   constructor(
     public content: string,
-    public userId: string,
+    public commentatorId: string,
     public postId: string,
   ) {}
 }
@@ -16,14 +18,32 @@ export class CreateCommentUseCase
 {
   constructor(
     protected usersQueryRepository: UsersQueryRepository,
+    protected postsQueryRepository: PostsQueryRepository,
     protected commentsRepository: CommentsRepository,
   ) {}
 
   async execute(command: CreateCommentCommand) {
-    const { userId, postId, content } = command;
-    const blogger = await this.usersQueryRepository.getUserById(userId);
+    const { commentatorId, postId, content } = command;
+
+    const commentator = await this.usersQueryRepository.getUserById(
+      commentatorId,
+    );
+    const post = await this.postsQueryRepository.getPostById(postId);
+    const blogOwnerId = await this.postsQueryRepository.getPostsBloggerId(
+      postId,
+    );
+    const createdComment: CreatedCommentDto = {
+      content,
+      commentatorId,
+      commentatorLogin: commentator.login,
+      blogId: post.blogId,
+      blogName: post.blogName,
+      blogOwnerId,
+      postId,
+      postTitle: post.title,
+    };
     const commentModel = await this.commentsRepository.createCommentModel();
-    commentModel.initial(content, userId, blogger.login, postId);
+    commentModel.initial(createdComment);
     await this.commentsRepository.save(commentModel);
     return await this.commentsRepository.save(commentModel);
   }
