1aeafe20c44c734a8bd28988e9e3847dcfc8d100
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-24 13:43:45 +0200

    fix error / add functional of HW16

--- src/blogs/providers/blogs.query.repository.ts
+++ src/blogs/providers/blogs.query.repository.ts
@@ -5,7 +5,8 @@ import { Blog, BlogDocument } from '../domain/blog.schema';
 import { pagesCount } from '../../common/helpers/helpers';
 import { BlogViewModel } from '../dto/view-models/blog.view.model';
 import { PaginatorViewModel } from '../../common/dto/view-models/paginator.view.model';
-import { BlogWithOwnerViewModel } from '../dto/view-models/blog-with-owner.view.model';
+import { BlogSaViewModel } from '../dto/view-models/blog-sa-view.model';
+import { PaginatorInputType } from '../../common/dto/input-models/paginator.input.type';
 
 @Injectable()
 export class BlogsQueryRepository {
@@ -18,22 +19,23 @@ export class BlogsQueryRepository {
   async findBlogs(
     paginatorParams,
     searchNameTerm,
-    options?: { ownerInclude?: boolean; userId?: string },
+    options?: { viewForSa?: boolean; userId?: string },
   ): Promise<PaginatorViewModel<BlogViewModel>> {
     const { sortBy, sortDirection, pageSize, pageNumber } = paginatorParams;
     let filter = searchNameTerm
       ? { name: { $regex: searchNameTerm, $options: 'i' } }
       : {};
     if (options?.userId) {
-      filter = { ...filter, ...{ 'blogOwnerInfo.userId': options.userId } };
+      filter = { ...filter, ...{ blogOwnerId: options.userId } };
     }
+
     const totalCount = await this.BlogModel.countDocuments(filter);
     const result = await this.BlogModel.find(filter)
       .sort({ [sortBy]: sortDirection })
       .skip((pageNumber - 1) * pageSize)
       .limit(pageSize);
-    const items: BlogViewModel[] = options?.ownerInclude
-      ? result.map((b) => this.getBlogViewModelWithOwner(b))
+    const items: BlogViewModel[] = options?.viewForSa
+      ? result.map((b) => this.getSaViewModelWithOwner(b))
       : result.map((b) => this.getBlogViewModel(b));
     return {
       pagesCount: pagesCount(totalCount, pageSize),
@@ -61,22 +63,101 @@ export class BlogsQueryRepository {
     };
   }
 
-  getBlogViewModelWithOwner(
-    blog: Blog,
-  ): BlogWithOwnerViewModel | BlogViewModel {
+  getSaViewModelWithOwner(blog: Blog): BlogSaViewModel | BlogViewModel {
     const blogView = this.getBlogViewModel(blog);
-    if (blog.blogOwnerInfo)
-      return { ...blogView, blogOwnerInfo: blog.blogOwnerInfo };
-    return blogView;
+    const banInfo = {
+      isBanned: blog.isBanned,
+      banDate: blog.banDate?.toString() || null,
+    };
+    const blogOwnerInfo = {
+      userId: blog.blogOwnerId,
+      userLogin: blog.blogOwnerLogin,
+    };
+    return { ...blogView, blogOwnerInfo, banInfo };
   }
 
   async getBlogOwner(blogId: string) {
     const blog = await this.BlogModel.findById(blogId);
-    return blog.blogOwnerInfo
+    return blog.blogOwnerId
       ? {
-          userId: blog.blogOwnerInfo.userId,
-          userLogin: blog.blogOwnerInfo.userLogin,
+          userId: blog.blogOwnerId,
+          userLogin: blog.blogOwnerLogin,
         }
       : null;
+  }
+
+  async getBannedUsers(
+    paginatorParams: PaginatorInputType,
+    searchLoginTerm: string,
+    blogId: string,
+  ) {
+    const blog = await this.BlogModel.findById(blogId);
+    const { sortBy, sortDirection, pageSize, pageNumber } = paginatorParams;
+    let users = blog.bannedUsers;
+    if (searchLoginTerm)
+      users = users.filter((u) => u.userLogin.includes(searchLoginTerm));
+    const totalCount = users.length;
+    console.log(`totalCount:${totalCount}`);
+    const pagesCount = Math.ceil(totalCount / pageSize);
+    console.log(`pagesCount:${pagesCount}`);
+    const firstIndex = pageSize * (pageNumber - 1);
+    console.log(`firstIndex:${firstIndex}`);
+    const lastIndex =
+      firstIndex + pageSize <= totalCount - 1
+        ? firstIndex + pageSize
+        : totalCount;
+    console.log(`lastIndex:${lastIndex}`);
+    let sortedUsers = [];
+    if (sortBy === 'createdAt') {
+      console.log('sort by createdAt');
+      sortedUsers = users.sort((a, b) => {
+        if (+a['banDate'] > +b['banDate']) {
+          console.log('1 case');
+          return sortDirection === 'asc' ? 1 : -1;
+        }
+        if (+a['banDate'] < +b['banDate']) {
+          console.log('2 case');
+          return sortDirection === 'asc' ? -1 : 1;
+        }
+        return 0;
+      });
+    }
+
+    if (['banReason', 'userLogin', 'userId'].includes(sortBy)) {
+      sortedUsers = users.sort((a, b) => {
+        if (a[sortBy] > b[sortBy]) return sortDirection === 'asc' ? 1 : -1;
+        if (a[sortBy] < b[sortBy]) return sortDirection === 'asc' ? -1 : 1;
+        return 0;
+      });
+    }
+
+    console.log(sortedUsers);
+
+    if (firstIndex > totalCount - 1) sortedUsers = [];
+    if (firstIndex <= totalCount)
+      sortedUsers = sortedUsers.slice(firstIndex, lastIndex);
+    const items = sortedUsers.map((e) => ({
+      id: e.userId,
+      login: e.userLogin,
+      banInfo: {
+        isBanned: true,
+        banDate: e.banDate,
+        banReason: e.banReason,
+      },
+    }));
+
+    return {
+      pagesCount,
+      page: pageNumber,
+      pageSize,
+      totalCount,
+      items,
+    };
+  }
+
+  async isUserBanned(userId: string, blogId: string) {
+    const blog = await this.BlogModel.findById(blogId);
+    const users = blog.bannedUsers.map((u) => u.userId);
+    return users.includes(userId);
   }
 }
