9a6aa30c5d609adb54e2a8af5fcf4a0d5c306ecb
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-02-24 13:46:26 +0200

    fix error / add functional of HW16

--- src/posts/providers/posts.repository.ts
+++ src/posts/providers/posts.repository.ts
@@ -20,19 +20,18 @@ export class PostsRepository {
     });
     return result.deletedCount === 1;
   }
-
   async getPostModelById(postId: string) {
     return this.PostModel.findById(postId);
   }
-
   async getPostsModelsByUserId(userId: string) {
     return this.PostModel.find({ bloggerId: userId });
   }
-
+  async getPostsModelsByBlogId(blogId: string) {
+    return this.PostModel.find({ blogId });
+  }
   async getPostsModelsByLikeUserId(userId: string) {
     return this.PostModel.find({ 'likes.userId': userId });
   }
-
   async save(createdPostModel) {
     const newPost = await createdPostModel.save();
     return newPost?._id?.toString();
